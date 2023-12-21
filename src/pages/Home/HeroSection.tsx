1ec27e20441853d2b8204cb1965a29044fa2dbcd
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-12-22 01:14:05 +0600

    Add HeroSection component with responsive design
    The HeroSection component features a responsive layout with a heading, subheading, call-to-action button, and smart integration icons. The component is designed to showcase the benefits of the application. The 'Button' component is used with the 'ButtonSize' enum for consistent styling across the application.

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Container from "../../components/common/Container";
+import { images } from "../../constant";
+import Button, { ButtonSize } from "../../components/common/Button";
+import { SiNotion, SiJira, SiSlack, SiDropbox } from "react-icons/si";
 
 const HeroSection: React.FC = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <Container>
+        <div className=" flex flex-col  md:flex-row gap-10 md:gap-0 justify-between items-center">
+          <div className=" md:w-5/12 py-20 flex flex-col gap-4 lg:gap-6 ">
+            <h1 className=" text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold lg:leading-[52px] xl:leading-[80px]">
+              Fast and Clean help structure your work
+            </h1>
+            <h4 className="text-lg lg:text-xl text-zinc-600">
+              Plan projects, stay on track, and deliver on time without
+              overworking your team
+            </h4>
+            <div className=" w-fit">
+              <Button size={ButtonSize.LG}>Let’s Explore</Button>
+            </div>
+            <div className="flex flex-col gap-3">
+              <h1 className=" font-semibold text-zinc-600">
+                Smart integration
+              </h1>
+              <div className=" flex justify-start items-center gap-6 text-3xl">
+                <SiSlack />
+                <SiDropbox />
+                <SiNotion />
+                <SiJira />
+              </div>
+            </div>
+          </div>
+          <div className="md:w-7/12 ">
+            <img src={images.HeroSection} alt="herosection" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HeroSection;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import Navbar from "../../components/Navbar";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <HeroSection />
+      <div className=" relative">
+        <div className="flex flex-col md:flex-row  justify-between">
+          <div className="md:w-8/12 h-[580px] md:h-[80vh] lg:h-[90vh] xl:h-[100vh]"></div>
+          <div className="md:w-4/12 bg-violet-50 h-[420px] md:h-[80vh] lg:h-[90vh] xl:h-[100vh]"></div>
+        </div>
+        <div className=" absolute h-screen top-0 left-0 right-0">
+          <div className=" w-full h-full">
+            <Navbar />
+            <HeroSection />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
