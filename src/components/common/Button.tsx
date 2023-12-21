3ba5d8a0230d79129eab2dc957e4de6c3cdf8ee4
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-12-22 01:15:39 +0600

    Refactor Button component to include size variants
    The Button component has been enhanced to include a size variant, allowing for better customization across the application. The 'ButtonSize' enum, introduced for consistency, offers three options: 'SM' (small), 'MD' (medium), and 'LG' (large). The component now dynamically adjusts its styling based on the specified size, providing more flexibility in button design. This update ensures a cleaner and more scalable implementation for button components throughout the application.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,12 +1,23 @@
 import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
+export enum ButtonSize {
+  SM = "sm",
+  MD = "md",
+  LG = "lg",
+}
+
 interface ButtonProps {
   children: ReactNode;
   url?: string;
+  size?: ButtonSize;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, url }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  url,
+  size = ButtonSize.MD,
+}) => {
   return url ? (
     <Link
       to={url}
@@ -14,7 +25,11 @@ const Button: React.FC<ButtonProps> = ({ children, url }) => {
       {children}
     </Link>
   ) : (
-    <button className=" px-3 py-2 bg-zinc-900 text-zinc-200 rounded-md font-semibold text-base">
+    <button
+      className={`
+      ${size === ButtonSize.MD && "px-3 text-base"}
+      ${size === ButtonSize.LG && "px-5 text-lg"}
+      py-2 bg-zinc-900 text-zinc-200 rounded-md font-semibold `}>
       {children}
     </button>
   );
